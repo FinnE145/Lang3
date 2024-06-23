ee876d0254d7df3194b38fe489cb8d48cb920092
FinnE145 <finn.emmerson@renertschool.ca>
2024-06-23 12:39:45 -0600

    add test tcp client and server

---
+++ VSCode Extension/Language Server/TCPServer/Server.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var hostName = Dns.GetHostName();
+IPHostEntry localhost = await Dns.GetHostEntryAsync(hostName);
+// This is the IP address of the local machine
+IPAddress localIpAddress = localhost.AddressList[0];
+IPEndPoint ipEndPoint = new(localIpAddress, 6500);
+
+Socket listener = new(
+    ipEndPoint.AddressFamily,
+    SocketType.Stream,
+    ProtocolType.Tcp);
+
+listener.Bind(ipEndPoint);
+listener.Listen(100);
+
+int msgCount = 0;
+
+var handler = await listener.AcceptAsync();
+while (true)
+{
+    // Receive message.
+    var buffer = new byte[1_024];
+    var received = await handler.ReceiveAsync(buffer, SocketFlags.None);
+    var response = Encoding.UTF8.GetString(buffer, 0, received);
+    
+    var eom = "\x04";
+    if (response.IndexOf(eom) > -1 /* is end of message */)
+    {
+        Console.WriteLine(
+            $"Socket server received message: \"{response.Replace(eom, "")}\"");
+
+        var ackMessage = "this can be anything right?" + msgCount;
+        msgCount++;
+        var echoBytes = Encoding.UTF8.GetBytes(ackMessage);
+        await handler.SendAsync(echoBytes, 0);
+        Console.WriteLine(
+            $"Socket server sent acknowledgment: \"{ackMessage}\"");
+
+        //break;
+    }
+}
