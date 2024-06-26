592c449fb23f2efab38183f1252ba9c954e0a59e
FinnE145 <finn.emmerson@renertschool.ca>
2024-06-25 21:32:10 -0600

    Add main runner script

---
+++ Lang3 Source/Lang3.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace Lang3;
+
+class Lang3Runner {
+    public static void Main(string[] args) {
+        string fp;
+        if (args.Length == 0) {
+            fp = "../Lang3 Test Files/test.l3";
+        } else {
+            fp = args[0];
+        }
+
+        string code = "";
+        try {
+            code = File.ReadAllText(fp) + "\n";
+        } catch (FileNotFoundException) {
+            throw new Exception($"File {fp} doesn't exist");
+        }
+
+        Console.Write(code);
+    }
+}
