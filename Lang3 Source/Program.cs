6b5aa5aa4282826188a292c563ba164408bfeed1
FinnE145 <finn.emmerson@renertschool.ca>
2024-07-08 09:38:57 -0600

    Make errors into a DynamicObject

--- Lang3 Source/Program.cs
+++ Lang3 Source/Program.cs
@@ -1,14 +1,11 @@
-﻿using System.Diagnostics;
-using Lang3.Utils;
-using Lang3.Utils.ErrorTypes;
-using static Lang3.Utils.Errors;
+﻿using Lang3.Utils;
 
 namespace Lang3;
 
 class Lang3Runner {
     public static void Main(string[] args) {
         Dictionary<string, string> files = [];
-        Errors err = new(files);
+        dynamic err = new Errors(files);
         string fp;
         if (args.Length == 0) {
             fp = "C:/Users/finne/OneDrive/Documents/0coding/Lang3_Ghub/Lang3 Test Files/test.l3";
@@ -21,17 +18,21 @@ class Lang3Runner {
         try {
             code = File.ReadAllText(fp) + "\n";
         } catch (FileNotFoundException) {
-            err.Raise(2, [fp, "found"]);
+            err.IOError(new string[] {fp, "found"});
+            //err.Raise(2, new List<string>([fp, "found"]));
             //throw new Exception($"File {fp} doesn't exist");
         } catch (DirectoryNotFoundException) {
-            err.Raise(2, [fp, "found"]);
+            err.IOError(new string[] {fp, "found"});
+            //err.Raise(2, new List<string>([fp, "found"]));
             //throw new Exception($"Directory {fp} doesn't exist");
         } catch (IOException) {
-            err.Raise(2, [fp, "accessed"]);
+            err.IOError(new string[] {fp, "accessed"});
+            //err.Raise(2, new List<string>([fp, "accessed"]));
             //throw new Exception($"Couldn't read file {fp}");
         }
 
         files.Add(fp, code);
-        err.Raise(0, null, fp);
+
+        err.NonexistentError();
     }
 }
--- Lang3 Source/Utils/Errors.cs
+++ Lang3 Source/Utils/Errors.cs
@@ -1,21 +1,45 @@
-using System.Diagnostics;
+using System.Dynamic;
 
 namespace Lang3.Utils;
 
-class Errors(Dictionary<string, string> files) {
+class Errors(Dictionary<string, string> files) : DynamicObject {
     private Dictionary<string, string> files = files;
-    static private readonly List<string> errorNames = [
-        "Success",  // 0 - None
-        "Error",    // 1 - Error
-        "IO Error"  // 2 - IOError
-    ];
-    static private readonly List<string> messageTemplates = [
+    private enum ErrorNames {
+        Success,    // 0 - None
+        Error,      // 1 - Error
+        IOError     // 2 - IOError
+    }
+    private readonly List<string> messageTemplates = [
         "This is not an error.",    // 0 - None
         "An error occurred.",       // 1 - Error
         "`{0}` could not be {1}."   // 2 - IOError
     ];
 
-    public int Raise(int errorCode, string[]? msgArgs, string? file = null, int? line = null, int? start = null, int? end = null, bool fatal = true) {
+    public override IEnumerable<string> GetDynamicMemberNames() {
+        return Enum.GetNames<ErrorNames>();
+    }
+
+    public override bool TryGetMember(GetMemberBinder binder, out object? result) {
+        if (!Enum.TryParse<ErrorNames>(binder.Name, out ErrorNames error)) {
+            result = null;
+            return false;
+        } else {
+            result = (int) error;
+            return true;
+        }
+    }
+
+    public override bool TryInvokeMember(InvokeMemberBinder binder, object?[]? args, out object? result) {
+        if (!Enum.TryParse<ErrorNames>(binder.Name, out ErrorNames error)) {
+            result = null;
+            return false;
+        } else {
+            result = error + "INVOKED";
+            return true;
+        }
+    }
+
+    private int Raise(int errorCode, string[]? msgArgs = null, string? file = null, int? line = null, int? start = null, int? end = null, bool fatal = true) {
         string? sample = null;
         if (file is not null) {
             files.TryGetValue(file, out sample);
@@ -23,11 +47,17 @@ class Errors(Dictionary<string, string> files) {
         sample = line is not null ? sample?.Split('\n')[(int) line] : sample;
 
         Console.ForegroundColor = ConsoleColor.Red;
-        Console.Write($"{errorNames[errorCode]} [{errorCode}]");
+        Console.Write($"{(ErrorNames) errorCode} [{errorCode}]");
         Console.ResetColor();
         Console.Write(": ");
 
-        string[] str = string.Format(messageTemplates[errorCode], msgArgs ?? []).Split('`');
+        string[] str;
+        try {
+            str = string.Format(messageTemplates[errorCode], msgArgs ?? []).Split('`');
+        } catch (FormatException) {
+            throw new ArgumentException("Message arguments do not match the template for that error.");
+        }
+        
         for (int i = 0; i < str.Length; i++) {
             Console.Write(str[i++]);
             Console.ForegroundColor = ConsoleColor.Green;
@@ -72,7 +102,7 @@ class Errors(Dictionary<string, string> files) {
         return errorCode;
     }
 
-    public int Raise(int errorCode, string[]? msgArgs, Lexer.Token[]? token) {
-        return Raise(errorCode, msgArgs, token?[0].file, token?[0].line, token?[0].start, token?[0].end);
+    private int Raise(int errorCode, string[]? msgArgs, Lexer.Token? token) {
+        return Raise(errorCode, msgArgs, token?.file, token?.line, token?.start, token?.end);
     }
 }
