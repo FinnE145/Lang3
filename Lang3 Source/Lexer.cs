ce5b5eec2b55ef10347e848453407981f0e7e63c
FinnE145 <finn.emmerson@renertschool.ca>
2024-07-11 08:40:11 -0600

    Lex numbers

--- Lang3 Source/Lexer.cs
+++ Lang3 Source/Lexer.cs
@@ -85,12 +85,56 @@ class Lexer(Dictionary<string, string> files) {
 
     internal static readonly char[] opStarts = ['+', '-', '*', '/', '%', '>', '<', '=', '&', '|', '^', '!'];
 
+    private Token BucketNum(string code, int line, int ld, string fp, ref int i) {
+        int start = i++;
+        string t = "int";
+        while (i-- < code.Length && (char.IsDigit(code[++i]) || code[i] == '.')) {
+            if (code[i++] == '.') {
+                if (t == "dec") {
+                    throw new Exception("Number cannot have two decimal points");
+                }
+                t = "dec";
+            }
+        }
+        if (code[--i] == '.') {
+            throw new Exception("Number cannot end with a decimal point");
+        }
+        return new Token(t, code[start..(i+1)], line, start-ld, i-ld+1, fp);
+    }
+
     public List<Token> Lex(string fp) {
+        int line = 1;
+        int ld = 0;
+
         files.TryGetValue(fp, out string? code);
         if (code is null) {
             throw new FileNotFoundException($"Tried to lex {fp} but it doesn't exist.");
         }
-        Console.WriteLine($"Lexing \n'{code.TrimEnd()}' from {fp}");
-        return [];
+
+        List<Token> tokens = [];
+
+        for (int i = 0; i < code.Length; i++) {
+            char c = code[i];
+
+            if (c == ' ' || c == '\t') {
+                continue;
+            }
+
+            if (c == '\n') {
+                line++;
+                ld = i+1;
+                continue;
+            }
+
+            if (char.IsDigit(c)) {
+                tokens.Add(BucketNum(code, line, ld, fp, ref i));
+            }
+
+            string v = c != '\n' ? c.ToString() : "\\n";
+        }
+
+        tokens.Add(new Token("EOF", "", line, code.Length, code.Length, fp));
+
+        return tokens;
     }
 }
